8e3fee4d67f6cdc9d56d98504b3d7a53023b7d02
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-12-14 13:42:38 +0530

    added player controller with CRUD

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,44 @@
+class PlayersController < ApplicationController
+	def index
+			@players = Player.all
+		end
+	
+		def new
+			@player = Player.new
+		end
+	
+		def create
+			@player = Player.new(player_params)
+			if @player.save
+				redirect_to players_path
+			else
+				render :new
+			end
+		end
+	
+		def edit
+			@player = Player.find(params[:id])
+		end
+	
+		def update
+			@player = Player.find(params[:id])
+			if @player.update(player_params)
+				redirect_to players_path
+			else
+				render :edit
+			end
+		end
+	
+		def destroy
+			@player = Player.find(params[:id])
+			@player.destroy
+			redirect_to players_path
+		end
+	
+		private
+	
+		def player_params
+			params.require(:player).permit(:name, :stats)
+		end
+	end
+end
