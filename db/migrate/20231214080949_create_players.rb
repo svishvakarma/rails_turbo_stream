c3825664af96da1c5537740e7734f74cec4cfb2f
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-12-14 13:40:22 +0530

    Added player model and intialized database postgres

---
+++ db/migrate/20231214080949_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.json :stats
+
+      t.timestamps
+    end
+  end
+end
